bcbdb46198a6f11525253c5169a15a77fc947c39
sienkashing <sks_sien@hotmail.com>
2014-10-02 20:27:51 +0800

    Part B - Completion
    Changed result query to simplified version and removed sub queries.
    
    Added filtering capability that receives filter input from home.php.

--- Results.php
+++ Results.php
@@ -7,29 +7,183 @@
 </head>
 
 <body>
+<a href="Home.php"><img src="Pictures/Logo.jpg" alt="" height="120" width="149" /></a>
 <table border="1">
-	<?php
+	<?php		
 		$con = mysqli_connect("localhost","root","","winestore");
-		$query = "SELECT ". 
+		
+		global $Where;
+		if (!empty($_GET['WineName']))
+		{
+			$WineName = mysqli_real_escape_string($con, $_GET['WineName']);		
+			$Where = "wine.wine_name Like '%".$WineName."%' ";
+		}
+		if (!empty($_GET['WineryName']))
+		{
+			$WineryName = mysqli_real_escape_string($con,$_GET['WineryName']);
+			if ($Where <> "")
+			{	
+				$Where = $Where." AND winery.winery_name LIKE '%".$WineryName."%' ";
+			}
+			else
+			{
+				$Where = "winery.winery_name LIKE '%".$WineryName."%' ";
+			}						
+		}
+		if (!empty($_GET['Region']) and ($_GET['Region'] <> 'All'))
+		{
+			$Region = mysqli_real_escape_string($con,$_GET['Region']);
+			if ($Where <> "")
+			{
+				$Where = $Where." AND region.region_name = '".$Region."' ";
+			}
+			else
+			{
+				$Where = "region.region_name = '".$Region."' ";
+			}			
+		}
+		if (!empty($_GET['MinStock']))
+		{
+			$MinStock = mysqli_real_escape_string($con,$_GET['MinStock']);
+			if ($Where <> "")
+			{
+				$Where = $Where." AND inventory.on_hand >= '".$MinStock."' ";
+			}
+			else
+			{
+				$Where = "inventory.on_hand >= '".$MinStock."' ";
+			}
+		}	
+		if (!empty($_GET['MinStock']))
+		{
+			$MinStock = mysqli_real_escape_string($con,$_GET['MinStock']);
+			if ($Where <> "")
+			{
+				$Where = $Where." AND inventory.on_hand >= '".$MinStock."' ";
+			}
+			else
+			{
+				$Where = "inventory.on_hand >= '".$MinStock."' ";
+			}
+		}
+		if (!empty($_GET['YearFrom']) and (!empty($_GET['YearTo'])))
+		{
+			$YearFrom = mysqli_real_escape_string($con,$_GET['YearFrom']);
+			$YearTo = mysqli_real_escape_string($con,$_GET['YearTo']);
+			if ($Where <> "")
+			{
+				$Where = $Where." AND wine.`year` BETWEEN '".$YearFrom."' AND '".$YearTo."' ";
+			}
+			else
+			{
+				$Where = "wine.`year` BETWEEN '".$YearFrom."' AND '".$YearTo."' ";
+			}
+		}	
+		else 
+		{
+			if (!empty($_GET['YearFrom']))
+			{
+				$YearFrom = mysqli_real_escape_string($con,$_GET['YearFrom']);
+				if ($Where <> "")
+				{
+					$Where = $Where." AND wine.`year` >='".$YearFrom."' ";
+				}
+				else
+				{
+					$Where = "wine.`year` >='".$YearFrom."' ";
+				}				
+			}
+			if (!empty($_GET['YearTo']))
+			{
+				$YearTo = mysqli_real_escape_string($con,$_GET['YearTo']);
+				if ($Where <> "")
+				{
+					$Where = $Where." AND wine.`year` <='".$YearTo."' ";
+				}
+				else
+				{
+					$Where = "wine.`year` <='".$YearTo."' ";
+				}				
+			}					
+		}	
+		if (!empty($_GET['CostFrom']) and (!empty($_GET['CostTo'])))
+		{
+			$CostFrom = mysqli_real_escape_string($con,$_GET['CostFrom']);
+			$CostTo = mysqli_real_escape_string($con,$_GET['CostTo']);
+			if ($Where <> "")
+			{
+				$Where = $Where." AND inventory.cost BETWEEN '".$CostFrom."' AND '".$CostTo."' ";
+			}
+			else
+			{
+				$Where = "inventory.cost BETWEEN '".$CostFrom."' AND '".$CostTo."' ";
+			}
+		}	
+		else 
+		{
+			if (!empty($_GET['CostFrom']))
+			{
+				$CostFrom = mysqli_real_escape_string($con,$_GET['CostFrom']);
+				if ($Where <> "")
+				{
+					$Where = $Where." AND inventory.cost >='".$CostFrom."' ";
+				}
+				else
+				{
+					$Where = "inventory.cost >='".$CostFrom."' ";
+				}				
+			}
+			if (!empty($_GET['CostTo']))
+			{
+				$CostTo = mysqli_real_escape_string($con,$_GET['CostTo']);
+				if ($Where <> "")
+				{
+					$Where = $Where." AND inventory.cost <='".$CostTo."' ";
+				}
+				else
+				{
+					$Where = "inventory.cost <='".$CostTo."' ";
+				}				
+			}					
+		}									
+		if ($Where <> "")
+		{
+			$Where = "WHERE ".$Where." ";
+		}
+				
+		$query = "SELECT ".
+			    "wine.wine_id AS WineID, ".
 				"wine.wine_name AS WineName, ".
 				"wine.`year` AS Year, ".
 				"grape_variety.variety AS Variety, ".				
 				"winery.winery_name AS Winery, ".
-				"region.region_name AS Region ".
+				"region.region_name AS Region, ".
+				"inventory.cost AS Cost, ".
+				"inventory.on_hand AS OnHand, ".
+				"Count(items.cust_id) AS TotalCustomers ".
 				"FROM ".
 				"wine ".
 				"INNER JOIN wine_variety ON wine.wine_id = wine_variety.wine_id ".
-				"INNER JOIN grape_variety ON wine_variety.variety_id = grape_variety.variety_id ".
 				"INNER JOIN winery ON wine.winery_id = winery.winery_id ".
+				"INNER JOIN grape_variety ON wine_variety.variety_id = grape_variety.variety_id ".
 				"INNER JOIN region ON winery.region_id = region.region_id ".
+				"INNER JOIN inventory ON wine.wine_id = inventory.wine_id ".
+				"INNER JOIN items ON wine.wine_id = items.wine_id ".				
+				$Where.
 				"GROUP BY ".
 				"wine.wine_name, ".
 				"grape_variety.variety, ".
 				"wine.`year`, ".
 				"winery.winery_name, ".
-				"winery.region_id ".
-				"ORDER BY WineName,Year,Variety,Winery, Region ";
-	    #echo $query;
+				"region.region_name, ".
+				"inventory.cost, ".
+				"inventory.on_hand ";
+				if (!empty($_GET['MinCustomers']))
+				{
+					$query .= "HAVING COUNT(items.cust_id) >= '".$_GET['MinCustomers']. "' ";
+				}				
+				$query .= "ORDER BY WineName,Year,Variety,Winery, Region ";
+	    echo $query;
 	    echo "</br>";
 		$result = mysqli_query($con,$query);
 		if (mysqli_num_rows($result) == 0)
@@ -44,12 +198,10 @@
 				"<th>Wine Variety</th>".				
 				"<th>Winery Name</th>".
 				"<th>Wine Region</th>".
-				"<th>Min. Cost of Wine</th>".
-				"<th>Bottles at Min. Cost</th>".
-				"<th>Total Bottles</th>".
-				"<th>No. Customers</th>".
-			"</tr>";
-			$tmpWineName = ""; #use for comparison to determine whether or not to run subqueries
+				"<th>Cost</th>".
+				"<th>On Hand</th>".
+				"<th>Total Orders</th>".
+			"</tr>";			
 			while($row = mysqli_fetch_array($result))
 			{								
 				echo "<tr>".
@@ -57,65 +209,11 @@
 					"<td>".$row['Year']."</td>".
 					"<td>".$row['Variety']."</td>".
 					"<td>".$row['Winery']."</td>".
-					"<td>".$row['Region']."</td>";
-					
-				#compare previous wine id with current row wine id, if different run sub queries for new wine id
-				if ($tmpWineName == "" || $tmpWineName <> $row['WineName'])
-				{
-					$tmpWineName = $row['WineName'];
-					
-					#Begin sub queries
-					#1 - Get Minimum cost of wine
-					$query = "SELECT Min(inventory.cost) AS MinCost ".
-							 "FROM inventory ".
-							 "INNER JOIN wine ON wine.winery_id = inventory.wine_id ".
-							 "WHERE wine.wine_name='".$tmpWineName."'";
-					$tmpresult = mysqli_query($con, $query);
-					while($tmprow =  mysqli_fetch_array($tmpresult))
-					{
-						$MinCost = $tmprow['MinCost'];
-					}
-						
-					#2 - Get bottles sold at Minimum cost
-					$query = "SELECT SUM(inventory.on_hand) AS MinCostBottles ".
-							 "FROM inventory ".
-							 "INNER JOIN wine ON wine.winery_id = inventory.wine_id ".
-						     "WHERE wine.wine_name= '".$tmpWineName."'".
-							 " AND inventory.cost = '".$MinCost."'";
-					$tmpresult = mysqli_query($con, $query);
-					while($tmprow =  mysqli_fetch_array($tmpresult))
-					{
-						$MinCostBottles = $tmprow['MinCostBottles'];
-					}
-					
-					#3 - Get Total bottles
-					$query = "SELECT SUM(inventory.on_hand) AS TotalBottles ".
-							 "FROM inventory ".
-							 "INNER JOIN wine ON wine.winery_id = inventory.wine_id ".
-							 "WHERE wine.wine_name='".$tmpWineName."'";
-					$tmpresult = mysqli_query($con, $query);
-					while($tmprow =  mysqli_fetch_array($tmpresult))
-					{
-						$TotalBottles = $tmprow['TotalBottles'];
-					}		
-					
-					#4 - Total Customers
-					$query = "SELECT Count(items.cust_id) AS TotalCustomers ".
-							 "FROM items ".
-							 "INNER JOIN wine ON wine.wine_id = items.wine_id ".
-							 "WHERE wine.wine_name='".$tmpWineName."'";
-					$tmpresult = mysqli_query($con, $query);
-					while($tmprow =  mysqli_fetch_array($tmpresult))
-					{
-						$TotalCustomers = $tmprow['TotalCustomers'];
-					}
-																						
-				}
-				echo "<td>".$MinCost."</td>";									
-				echo "<td>".$MinCostBottles."</td>";
-				echo "<td>".$TotalBottles."</td>";
-				echo "<td>".$TotalCustomers."</td>";
-				echo "</tr>";
+					"<td>".$row['Region']."</td>".
+					"<td>".$row['Cost']."</td>".
+				    "<td>".$row['OnHand']."</td>".
+				    "<td>".$row['TotalCustomers']."</td>".
+				    "</tr>";
 			}			
 		}
 		mysqli_close($con);
