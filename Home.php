5673cbd36ecdce5e617cc4bc3fd663ab25ef9f1b
sienkashing <sks_sien@hotmail.com>
2014-10-10 09:32:32 +0800

    Added Boot Strap CSS
    Added bootstrap CSS for better styling and standardized validation for
    wine name and winery name to prevent special characters from being
    entered.

--- Home.php
+++ Home.php
@@ -6,6 +6,18 @@
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 <title>Wine Store</title>
 <script type="text/javascript">
+function preventSpecialCharacter(evt)
+{
+ var charCode = (evt.which) ? evt.which : event.keyCode
+ if (charCode == 32 || charCode == 8 || charCode == 13 ||
+ 	((charCode > 47) && (charCode < 58)) ||
+ 	((charCode > 64) && (charCode < 91)) ||
+ 	((charCode > 96) && (charCode < 123)))
+    return true;
+
+ return false;
+}
+
 function isNumberKey(evt)
 {
  var charCode = (evt.which) ? evt.which : event.keyCode
@@ -41,56 +53,94 @@ function validate()
 	}
 }
 </script>
+<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
-
 <body>
-<img src="Pictures/Logo.jpg" alt="" height="120" width="149" />
-<form name="searchForm" action="Results.php" method="get" onsubmit="return validate()">
-<table style="width: 500px">
-	<tr>
-		<td>Wine Name:</td>
-		<td><input name="WineName" size ="30" type="text" /></td>
-	</tr>
-	<tr>
-		<td>Winery Name:</td>
-		<td><input name="WineryName" size="30" type="text" /></td>
-	</tr>	
-	<tr>
-		<td>Region:</td>
-		<td><select name="Region">
-				<?php
-					$con = mysqli_connect("localhost","root","","winestore");
-					$result = mysqli_query($con,"SELECT * FROM Region");
-					while($row = mysqli_fetch_array($result))
-					{
-						echo "<option>".$row['region_name']."</option>";		
-					}
-				?>				
-			</select>
-		</td>
-	</tr>
-	<tr>
-		<td>Min. Stock of Wine</td>
-		<td><input name="MinStock" type="text" size="19" onkeypress="return isNumberKey(event)"/></td>
-	</tr>
-	<tr>
-		<td>Min. No of Customers who bought a Wine:</td>
-		<td><input name="MinCustomers" type="text" size="19" onkeypress="return isNumberKey(event)"/></td>
-	</tr>
-	<tr>
-		<td>Wine Year:</td>
-		<td>From: <input name="YearFrom" type="text" maxlength="4" size="4" onkeypress="return isNumberKey(event)"/>To:<input name="YearTo" type="text" maxlength="4" size="4"/></td>
-	</tr>	
-	<tr>
-		<td>Cost (Dolar):</td>
-		<td>From: <input name="CostFrom" type="text" maxlength="4" size="4" onkeypress="return isNumberKey(event)"/>To:<input name="CostTo" type="text" maxlength="4" size="4"/></td>
-	</tr>
-	<tr>
-		<td>&nbsp;</td>
-		<td><input name="Search" type="submit" value="Search" /></td>
-	</tr>
-</table>
-</form>
+<div class="container">
+    <div class="col-xs-offset-3" id="body">
+        <img src="Pictures/Logo.jpg" alt="" height="120" width="400" />
+        <form name="searchForm" action="Results.php" method="get" onsubmit="return validate()" role="form" class="form-horizontal">
+                <div class="form-group">
+                        <label for="WineName" class="col-xs-2 control-label">Wine Name:</label>
+                        <div class="col-xs-5">                        
+                             <input class="form-control" id="WineName" name="WineName" type="text" placeholder="Wine Name"
+                                    onkeypress="return preventSpecialCharacter(event)"/>
+                        </div>
+                </div>
+                <div class="form-group">
+                        <label for="WineryName" class="col-xs-2 control-label">Winery Name:</label>
+                        <div class="col-xs-5">
+                            <input id="WineryName" name="WineryName" type="text" class="form-control" placeholder="Winery Name"
+                                   onkeypress="return preventSpecialCharacter(event)"/>
+                        </div>
+                </div>	
+                <div class="form-group">
+                        <label for="Region" class="col-xs-2 control-label">Region:</label>
+                        <div class="col-xs-3">
+                            <select id="Region" name="Region" class="form-control">
+                                <?php
+                                        $con = mysqli_connect("localhost","root","","winestore");
+                                        $result = mysqli_query($con,"SELECT * FROM Region");
+                                        while($row = mysqli_fetch_array($result))
+                                        {
+                                                echo "<option>".$row['region_name']."</option>";		
+                                        }
+                                ?>				
+                            </select>
+                        </div>
+                </div>
+                <div class="form-group">
+                        <label for="MinStock" class="col-xs-2 control-label">Min. Stock of Wine</label>
+                        <div class="col-xs-2">
+                            <input name="MinStock" type="text" onkeypress="return isNumberKey(event)" 
+                                   class="form-control" placeholder="Min Stock"/>
+                        </div>
+                </div>
+                <div class="form-group">
+                        <label for="MinCustomers" class="col-xs-2 control-label">Min. Customer Orders:</label>
+                        <div class="col-xs-2">
+                            <input id="MinCustomers" name="MinCustomers" type="text" onkeypress="return isNumberKey(event)"
+                                   class="form-control" placeholder="Min. Cust. Orders"/>
+                        </div>
+                </div>
+                <div class="form-group">
+                    <label for="YearFrom" class="col-xs-2 control-label">Wine Year:</label>
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <input id="YearFrom" name="YearFrom" type="text" maxlength="4" onkeypress="return isNumberKey(event)"
+                                   class="form-control" placeholder="From"/>                        
+                        </div>
+                        <div class="col-xs-2">
+                            <input name="YearTo" type="text" maxlength="4" onkeypress="return isNumberKey(event)"
+                                   class="form-control" placeholder="To"/>                        
+                        </div>
+                    </div>
+                </div>	
+                <div class="form-group">
+                        <label for="CostFrom" class="col-xs-2 control-label">Cost (Dolar):</label>
+                        <div class="row">
+                            <div class="col-xs-2">
+                                <input name="CostFrom" type="text" maxlength="4" onkeypress="return isNumberKey(event)"
+                                       class="form-control" placeholder="From"/>    
+                            </div>                        
+                            <div class="col-xs-2">
+                                <input name="CostTo" type="text" maxlength="4" onkeypress="return isNumberKey(event)"
+                                       class="form-control" placeholder="To"/>    
+                            </div>                                            
+                        </div>                        
+                </div>
+                <div class="form-group">                    
+                    <div class="col-xs-2 col-xs-offset-2">
+                        <button type="submit" class="btn btn-default">Start Search</button>
+                    </div>                                    
+                </div>
+        </form>        
+    </div>
+</div> >
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- Results.php
+++ Results.php
@@ -4,221 +4,230 @@
 <head>
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 <title>Results</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
-
 <body>
-<a href="Home.php"><img src="Pictures/Logo.jpg" alt="" height="120" width="149" /></a>
-<table border="1">
-	<?php		
-		$con = mysqli_connect("localhost","root","","winestore");
-		
-		global $Where;
-		if (!empty($_GET['WineName']))
-		{
-			$WineName = mysqli_real_escape_string($con, $_GET['WineName']);		
-			$Where = "wine.wine_name Like '%".$WineName."%' ";
-		}
-		if (!empty($_GET['WineryName']))
-		{
-			$WineryName = mysqli_real_escape_string($con,$_GET['WineryName']);
-			if ($Where <> "")
-			{	
-				$Where = $Where." AND winery.winery_name LIKE '%".$WineryName."%' ";
-			}
-			else
-			{
-				$Where = "winery.winery_name LIKE '%".$WineryName."%' ";
-			}						
-		}
-		if (!empty($_GET['Region']) and ($_GET['Region'] <> 'All'))
-		{
-			$Region = mysqli_real_escape_string($con,$_GET['Region']);
-			if ($Where <> "")
-			{
-				$Where = $Where." AND region.region_name = '".$Region."' ";
-			}
-			else
-			{
-				$Where = "region.region_name = '".$Region."' ";
-			}			
-		}
-		if (!empty($_GET['MinStock']))
-		{
-			$MinStock = mysqli_real_escape_string($con,$_GET['MinStock']);
-			if ($Where <> "")
-			{
-				$Where = $Where." AND inventory.on_hand >= '".$MinStock."' ";
-			}
-			else
-			{
-				$Where = "inventory.on_hand >= '".$MinStock."' ";
-			}
-		}	
-		if (!empty($_GET['MinStock']))
-		{
-			$MinStock = mysqli_real_escape_string($con,$_GET['MinStock']);
-			if ($Where <> "")
-			{
-				$Where = $Where." AND inventory.on_hand >= '".$MinStock."' ";
-			}
-			else
-			{
-				$Where = "inventory.on_hand >= '".$MinStock."' ";
-			}
-		}
-		if (!empty($_GET['YearFrom']) and (!empty($_GET['YearTo'])))
-		{
-			$YearFrom = mysqli_real_escape_string($con,$_GET['YearFrom']);
-			$YearTo = mysqli_real_escape_string($con,$_GET['YearTo']);
-			if ($Where <> "")
-			{
-				$Where = $Where." AND wine.`year` BETWEEN '".$YearFrom."' AND '".$YearTo."' ";
-			}
-			else
-			{
-				$Where = "wine.`year` BETWEEN '".$YearFrom."' AND '".$YearTo."' ";
-			}
-		}	
-		else 
-		{
-			if (!empty($_GET['YearFrom']))
-			{
-				$YearFrom = mysqli_real_escape_string($con,$_GET['YearFrom']);
-				if ($Where <> "")
-				{
-					$Where = $Where." AND wine.`year` >='".$YearFrom."' ";
-				}
-				else
-				{
-					$Where = "wine.`year` >='".$YearFrom."' ";
-				}				
-			}
-			if (!empty($_GET['YearTo']))
-			{
-				$YearTo = mysqli_real_escape_string($con,$_GET['YearTo']);
-				if ($Where <> "")
-				{
-					$Where = $Where." AND wine.`year` <='".$YearTo."' ";
-				}
-				else
-				{
-					$Where = "wine.`year` <='".$YearTo."' ";
-				}				
-			}					
-		}	
-		if (!empty($_GET['CostFrom']) and (!empty($_GET['CostTo'])))
-		{
-			$CostFrom = mysqli_real_escape_string($con,$_GET['CostFrom']);
-			$CostTo = mysqli_real_escape_string($con,$_GET['CostTo']);
-			if ($Where <> "")
-			{
-				$Where = $Where." AND inventory.cost BETWEEN '".$CostFrom."' AND '".$CostTo."' ";
-			}
-			else
-			{
-				$Where = "inventory.cost BETWEEN '".$CostFrom."' AND '".$CostTo."' ";
-			}
-		}	
-		else 
-		{
-			if (!empty($_GET['CostFrom']))
-			{
-				$CostFrom = mysqli_real_escape_string($con,$_GET['CostFrom']);
-				if ($Where <> "")
-				{
-					$Where = $Where." AND inventory.cost >='".$CostFrom."' ";
-				}
-				else
-				{
-					$Where = "inventory.cost >='".$CostFrom."' ";
-				}				
-			}
-			if (!empty($_GET['CostTo']))
-			{
-				$CostTo = mysqli_real_escape_string($con,$_GET['CostTo']);
-				if ($Where <> "")
-				{
-					$Where = $Where." AND inventory.cost <='".$CostTo."' ";
-				}
-				else
-				{
-					$Where = "inventory.cost <='".$CostTo."' ";
-				}				
-			}					
-		}									
-		if ($Where <> "")
-		{
-			$Where = "WHERE ".$Where." ";
-		}
-				
-		$query = "SELECT ".
-			    "wine.wine_id AS WineID, ".
-				"wine.wine_name AS WineName, ".
-				"wine.`year` AS Year, ".
-				"grape_variety.variety AS Variety, ".				
-				"winery.winery_name AS Winery, ".
-				"region.region_name AS Region, ".
-				"inventory.cost AS Cost, ".
-				"inventory.on_hand AS OnHand, ".
-				"Count(items.cust_id) AS TotalCustomers ".
-				"FROM ".
-				"wine ".
-				"INNER JOIN wine_variety ON wine.wine_id = wine_variety.wine_id ".
-				"INNER JOIN winery ON wine.winery_id = winery.winery_id ".
-				"INNER JOIN grape_variety ON wine_variety.variety_id = grape_variety.variety_id ".
-				"INNER JOIN region ON winery.region_id = region.region_id ".
-				"INNER JOIN inventory ON wine.wine_id = inventory.wine_id ".
-				"INNER JOIN items ON wine.wine_id = items.wine_id ".				
-				$Where.
-				"GROUP BY ".
-				"wine.wine_name, ".
-				"grape_variety.variety, ".
-				"wine.`year`, ".
-				"winery.winery_name, ".
-				"region.region_name, ".
-				"inventory.cost, ".
-				"inventory.on_hand ";
-				if (!empty($_GET['MinCustomers']))
-				{
-					$query .= "HAVING COUNT(items.cust_id) >= '".$_GET['MinCustomers']. "' ";
-				}				
-				$query .= "ORDER BY WineName,Year,Variety,Winery, Region ";
-	    #echo $query;
-	    echo "</br>";
-		$result = mysqli_query($con,$query);
-		if (mysqli_num_rows($result) == 0)
-		{
-			echo 'No records match your search criteria.';
-		}
-		else
-		{
-			echo "<tr>".
-				"<th>Wine Name</th>".
-				"<th>Year</th>".				
-				"<th>Wine Variety</th>".				
-				"<th>Winery Name</th>".
-				"<th>Wine Region</th>".
-				"<th>Cost</th>".
-				"<th>On Hand</th>".
-				"<th>Total Orders</th>".
-			"</tr>";			
-			while($row = mysqli_fetch_array($result))
-			{								
-				echo "<tr>".
-					"<td>".$row['WineName']."</td>".
-					"<td>".$row['Year']."</td>".
-					"<td>".$row['Variety']."</td>".
-					"<td>".$row['Winery']."</td>".
-					"<td>".$row['Region']."</td>".
-					"<td>".$row['Cost']."</td>".
-				    "<td>".$row['OnHand']."</td>".
-				    "<td>".$row['TotalCustomers']."</td>".
-				    "</tr>";
-			}			
-		}
-		mysqli_close($con);
-	?>
-</table>
+<div class="container">    
+    <a href="Home.php"><img src="Pictures/Logo.jpg" alt="" height="120" width="400" /></a>
+    
+    <?php		
+            $con = mysqli_connect("localhost","root","","winestore");
+
+            global $Where;
+            if (!empty($_GET['WineName']))
+            {
+                    $WineName = mysqli_real_escape_string($con, $_GET['WineName']);		
+                    $Where = "wine.wine_name Like '%".$WineName."%' ";
+            }
+            if (!empty($_GET['WineryName']))
+            {
+                    $WineryName = mysqli_real_escape_string($con,$_GET['WineryName']);
+                    if ($Where <> "")
+                    {	
+                            $Where = $Where." AND winery.winery_name LIKE '%".$WineryName."%' ";
+                    }
+                    else
+                    {
+                            $Where = "winery.winery_name LIKE '%".$WineryName."%' ";
+                    }						
+            }
+            if (!empty($_GET['Region']) and ($_GET['Region'] <> 'All'))
+            {
+                    $Region = mysqli_real_escape_string($con,$_GET['Region']);
+                    if ($Where <> "")
+                    {
+                            $Where = $Where." AND region.region_name = '".$Region."' ";
+                    }
+                    else
+                    {
+                            $Where = "region.region_name = '".$Region."' ";
+                    }			
+            }
+            if (!empty($_GET['MinStock']))
+            {
+                    $MinStock = mysqli_real_escape_string($con,$_GET['MinStock']);
+                    if ($Where <> "")
+                    {
+                            $Where = $Where." AND inventory.on_hand >= '".$MinStock."' ";
+                    }
+                    else
+                    {
+                            $Where = "inventory.on_hand >= '".$MinStock."' ";
+                    }
+            }	
+            if (!empty($_GET['MinStock']))
+            {
+                    $MinStock = mysqli_real_escape_string($con,$_GET['MinStock']);
+                    if ($Where <> "")
+                    {
+                            $Where = $Where." AND inventory.on_hand >= '".$MinStock."' ";
+                    }
+                    else
+                    {
+                            $Where = "inventory.on_hand >= '".$MinStock."' ";
+                    }
+            }
+            if (!empty($_GET['YearFrom']) and (!empty($_GET['YearTo'])))
+            {
+                    $YearFrom = mysqli_real_escape_string($con,$_GET['YearFrom']);
+                    $YearTo = mysqli_real_escape_string($con,$_GET['YearTo']);
+                    if ($Where <> "")
+                    {
+                            $Where = $Where." AND wine.`year` BETWEEN '".$YearFrom."' AND '".$YearTo."' ";
+                    }
+                    else
+                    {
+                            $Where = "wine.`year` BETWEEN '".$YearFrom."' AND '".$YearTo."' ";
+                    }
+            }	
+            else 
+            {
+                    if (!empty($_GET['YearFrom']))
+                    {
+                            $YearFrom = mysqli_real_escape_string($con,$_GET['YearFrom']);
+                            if ($Where <> "")
+                            {
+                                    $Where = $Where." AND wine.`year` >='".$YearFrom."' ";
+                            }
+                            else
+                            {
+                                    $Where = "wine.`year` >='".$YearFrom."' ";
+                            }				
+                    }
+                    if (!empty($_GET['YearTo']))
+                    {
+                            $YearTo = mysqli_real_escape_string($con,$_GET['YearTo']);
+                            if ($Where <> "")
+                            {
+                                    $Where = $Where." AND wine.`year` <='".$YearTo."' ";
+                            }
+                            else
+                            {
+                                    $Where = "wine.`year` <='".$YearTo."' ";
+                            }				
+                    }					
+            }	
+            if (!empty($_GET['CostFrom']) and (!empty($_GET['CostTo'])))
+            {
+                    $CostFrom = mysqli_real_escape_string($con,$_GET['CostFrom']);
+                    $CostTo = mysqli_real_escape_string($con,$_GET['CostTo']);
+                    if ($Where <> "")
+                    {
+                            $Where = $Where." AND inventory.cost BETWEEN '".$CostFrom."' AND '".$CostTo."' ";
+                    }
+                    else
+                    {
+                            $Where = "inventory.cost BETWEEN '".$CostFrom."' AND '".$CostTo."' ";
+                    }
+            }	
+            else 
+            {
+                    if (!empty($_GET['CostFrom']))
+                    {
+                            $CostFrom = mysqli_real_escape_string($con,$_GET['CostFrom']);
+                            if ($Where <> "")
+                            {
+                                    $Where = $Where." AND inventory.cost >='".$CostFrom."' ";
+                            }
+                            else
+                            {
+                                    $Where = "inventory.cost >='".$CostFrom."' ";
+                            }				
+                    }
+                    if (!empty($_GET['CostTo']))
+                    {
+                            $CostTo = mysqli_real_escape_string($con,$_GET['CostTo']);
+                            if ($Where <> "")
+                            {
+                                    $Where = $Where." AND inventory.cost <='".$CostTo."' ";
+                            }
+                            else
+                            {
+                                    $Where = "inventory.cost <='".$CostTo."' ";
+                            }				
+                    }					
+            }									
+            if ($Where <> "")
+            {
+                    $Where = "WHERE ".$Where." ";
+            }
+
+            $query = "SELECT ".
+                        "wine.wine_id AS WineID, ".
+                            "wine.wine_name AS WineName, ".
+                            "wine.`year` AS Year, ".
+                            "grape_variety.variety AS Variety, ".				
+                            "winery.winery_name AS Winery, ".
+                            "region.region_name AS Region, ".
+                            "inventory.cost AS Cost, ".
+                            "inventory.on_hand AS OnHand, ".
+                            "Count(items.cust_id) AS TotalCustomers ".
+                            "FROM ".
+                            "wine ".
+                            "INNER JOIN wine_variety ON wine.wine_id = wine_variety.wine_id ".
+                            "INNER JOIN winery ON wine.winery_id = winery.winery_id ".
+                            "INNER JOIN grape_variety ON wine_variety.variety_id = grape_variety.variety_id ".
+                            "INNER JOIN region ON winery.region_id = region.region_id ".
+                            "INNER JOIN inventory ON wine.wine_id = inventory.wine_id ".
+                            "INNER JOIN items ON wine.wine_id = items.wine_id ".				
+                            $Where.
+                            "GROUP BY ".
+                            "wine.wine_name, ".
+                            "grape_variety.variety, ".
+                            "wine.`year`, ".
+                            "winery.winery_name, ".
+                            "region.region_name, ".
+                            "inventory.cost, ".
+                            "inventory.on_hand ";
+                            if (!empty($_GET['MinCustomers']))
+                            {
+                                    $query .= "HAVING COUNT(items.cust_id) >= '".$_GET['MinCustomers']. "' ";
+                            }				
+                            $query .= "ORDER BY WineName,Year,Variety,Winery, Region ";
+        #echo $query;
+        echo "</br>";
+            $result = mysqli_query($con,$query);
+            if (mysqli_num_rows($result) == 0)
+            {
+                    echo '<h1>No records match your search criteria.</h1>';
+            }
+            else
+            {
+                    echo '<table class="table table-striped">'.
+                            '<thead>'.
+                            '<tr>'.
+                            "<th>Wine Name</th>".
+                            "<th>Year</th>".				
+                            "<th>Wine Variety</th>".				
+                            "<th>Winery Name</th>".
+                            "<th>Wine Region</th>".
+                            "<th>Cost</th>".
+                            "<th>On Hand</th>".
+                            "<th>Total Orders</th>".
+                    "</tr></thead>";
+                    echo"<tbody>";
+                    while($row = mysqli_fetch_array($result))
+                    {								
+                            echo "<tr>".
+                                    "<td>".$row['WineName']."</td>".
+                                    "<td>".$row['Year']."</td>".
+                                    "<td>".$row['Variety']."</td>".
+                                    "<td>".$row['Winery']."</td>".
+                                    "<td>".$row['Region']."</td>".
+                                    "<td>".$row['Cost']."</td>".
+                                "<td>".$row['OnHand']."</td>".
+                                "<td>".$row['TotalCustomers']."</td>".
+                                "</tr>";
+                    }		
+                    echo "</tbody></table>";
+            }
+            mysqli_close($con);
+    ?>            
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
